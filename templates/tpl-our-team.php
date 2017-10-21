1697b09c3b1996c83af6bf3afe3710fbd8c5ccc9
Ken Vilar <kenvilar@gmail.com>
2017-10-21 18:32:02 +0800

    Our team template

---
+++ templates/tpl-our-team.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Template Name: Our Team
+ */
+get_header(); ?>
+
+<section id="template-container" class="our-team">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Joe-Cicci.png'; ?>" alt="team">
+				<p>Joe Cicci
+					<br><span>Owner</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Shannon-Harvey.png'; ?>" alt="team">
+				<p>Shannon Harvey
+					<br><span>Controller</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Alfred-Cicci.png'; ?>" alt="team">
+				<p>Alfred Cicci
+					<br><span>Owner</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Darlene-Cushman.png'; ?>" alt="team">
+				<p>Darlene Cushman
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Stephanie-Alant.png'; ?>" alt="team">
+				<p>Stephanie Alant
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/RoseAnne-Sutton.png'; ?>" alt="team">
+				<p>RoseAnne Sutton
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Danielle-Finley.png'; ?>" alt="team">
+				<p>Danielle Finley
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Lydia-Benscher.png'; ?>" alt="team">
+				<p>Lydia Benscher
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Melissa-Coon.png'; ?>" alt="team">
+				<p>Melissa Coon
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Cheryl-Teal.png'; ?>" alt="team">
+				<p>Cheryl Teal
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Stephanie-VanDusen.png'; ?>" alt="team">
+				<p>Stephanie VanDusen
+					<br><span>Graphic Designer & Marketing Manager</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Jay-Mancarella.png'; ?>" alt="team">
+				<p>Jay Mancarella
+					<br><span>Sales Representative</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Terry-Hood.png'; ?>" alt="team">
+				<p>Terry Hood
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Christie-Dixon.png'; ?>" alt="team">
+				<p>Christie Dixon
+					<br><span>Kitchen & Bath Design Consultant</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Bob-Mariano.png'; ?>" alt="team">
+				<p>Bob Mariano
+					<br><span>Installation Manager</span></p>
+			</div>
+			<div class="col-md-3 col-sm-12 equal-height single-team">
+				<img src="<?php echo get_template_directory_uri() . '/assets/images/team/Name.png'; ?>" alt="team">
+				<p>Name
+					<br><span>Position</span></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_footer(); ?>
+
