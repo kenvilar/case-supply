119f205160a68b318d04c9e49cb47cafaba0ef86
Ken Vilar <kenvilar@gmail.com>
2017-10-22 09:35:12 +0800

    Mobile service page template

--- templates/tpl-services.php
+++ templates/tpl-services.php
@@ -10,65 +10,71 @@ get_header(); ?>
 			<div class="col-md-offset-1 col-md-10">
 				<div class="col-md-6 col-sm-12 equal-height">
 					<div class="col-md-2">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
+						<img class="service-icon"
+						     src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
 						     alt="icon">
 					</div>
 					<div class="col-md-10">
-						<h4>In-Home Design</h4>
+						<h4 class="service-title">In-Home Design</h4>
 						<p>Our certified designers will come to you in the comfort of your home to design the kitchen or
 							bath of your dreams.</p>
 					</div>
 				</div>
 				<div class="col-md-6 col-sm-12 equal-height">
 					<div class="col-md-2">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
+						<img class="service-icon"
+						     src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
 						     alt="icon">
 					</div>
 					<div class="col-md-10">
-						<h4>3D Perspective Drawings</h4>
+						<h4 class="service-title">3D Perspective Drawings</h4>
 						<p>Case Supply’s advanced software system provides you with a realistic drawing of your
 							space.</p>
 					</div>
 				</div>
 				<div class="col-md-6 col-sm-12 equal-height">
 					<div class="col-md-2">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
+						<img class="service-icon"
+						     src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
 						     alt="icon">
 					</div>
 					<div class="col-md-10">
-						<h4>Installation</h4>
+						<h4 class="service-title">Installation</h4>
 						<p>Case Supply’s professional contractors will complete your project to perfection.</p>
 					</div>
 				</div>
 				<div class="col-md-6 col-sm-12 equal-height">
 					<div class="col-md-2">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
+						<img class="service-icon"
+						     src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
 						     alt="icon">
 					</div>
 					<div class="col-md-10">
-						<h4>Field Measure</h4>
+						<h4 class="service-title">Field Measure</h4>
 						<p>Prior to your order placement, Case Supply will measure your project to ensure your products
 							are ordered correctly.</p>
 					</div>
 				</div>
 				<div class="col-md-6 col-sm-12 equal-height">
 					<div class="col-md-2">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
+						<img class="service-icon"
+						     src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
 						     alt="icon">
 					</div>
 					<div class="col-md-10">
-						<h4>Job-Site Delivery</h4>
+						<h4 class="service-title">Job-Site Delivery</h4>
 						<p>Our experts will make sure your products are delivered safely and on time to your
 							doorstep.</p>
 					</div>
 				</div>
 				<div class="col-md-6 col-sm-12 equal-height">
 					<div class="col-md-2">
-						<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
+						<img class="service-icon"
+						     src="<?php echo get_template_directory_uri() . '/assets/images/icons/services/in-home-design.png'; ?>"
 						     alt="icon">
 					</div>
 					<div class="col-md-10">
-						<h4>Financing</h4>
+						<h4 class="service-title">Financing</h4>
 						<p>Select from a number of payment options to ensure your project’s affordability.</p>
 					</div>
 				</div>
