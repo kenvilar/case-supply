5a107e5defe81f9c42d8b16edd86753c4820fea6
Ken Vilar <kenvilar@gmail.com>
2017-10-22 21:44:59 +0800

    Register font-awesome styles and enqueues it

--- inc/wp-enqueue-script.php
+++ inc/wp-enqueue-script.php
@@ -4,8 +4,13 @@
  * Enqueue styles.
  */
 function casesupply_styles() {
-	wp_enqueue_style( 'casesupply-bootstrap-css',
+	wp_enqueue_style( 'casesupply-bootstrap',
 		get_stylesheet_directory_uri() . '/assets/css/bootstrap.min.css',
+		'',
+		'2017' );
+	
+	wp_enqueue_style( 'casesupply-font-awesome',
+		get_stylesheet_directory_uri() . '/assets/css/font-awesome.min.css',
 		'',
 		'2017' );
 	
@@ -31,34 +36,34 @@ function casesupply_scripts() {
 		array(),
 		'2017', true );
 	
-	wp_enqueue_script( 'casesupply-bootstrap-js',
+	wp_enqueue_script( 'casesupply-bootstrap',
 		get_template_directory_uri() . '/assets/js/bootstrap.min.js',
 		array( 'jquery' ),
 		'2017', true );
 	
-	wp_enqueue_script( 'casesupply-matchheight-js',
+	wp_enqueue_script( 'casesupply-matchheight',
 		get_template_directory_uri() . '/assets/lib/jquery.matchHeight-min.js',
-		array( 'jquery' ),
-		'2017', true );
-	
-	wp_enqueue_script( 'casesupply-main-js',
-		get_template_directory_uri() . '/assets/js/main.js',
 		array( 'jquery' ),
 		'2017', true );
 	
 	// Only display on Contact page
 	if ( is_page( 'contact' ) ) {
-		wp_enqueue_script( 'casesupply-custom-map-js',
+		wp_enqueue_script( 'casesupply-custom-map',
 			get_template_directory_uri() . '/assets/js/map.js',
 			array(),
 			'2017', true );
 		
-		wp_enqueue_script( 'casesupply-api-map-js',
+		wp_enqueue_script( 'casesupply-api-map',
 			'//maps.googleapis.com/maps/api/js?key=AIzaSyCLI2mmf0Q6SK6-_v4BT6xtizZgz06QWi0&callback=initMap',
 			array( 'casesupply-custom-map-js' ),
 			'2017', true );
 	}
 	// End only display on Contact page
+	
+	wp_enqueue_script( 'casesupply-main',
+		get_template_directory_uri() . '/assets/js/main.js',
+		array( 'jquery' ),
+		'2017', true );
 	
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
