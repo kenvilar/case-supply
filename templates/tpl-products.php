4855c15ac44c23024e2714c97407ab586e57a59e
Ken Vilar <kenvilar@gmail.com>
2017-10-22 11:08:02 +0800

    Fix products page template

--- templates/tpl-products.php
+++ templates/tpl-products.php
@@ -21,7 +21,7 @@ get_header(); ?>
 			<div class="col-md-offset-1 col-md-10 col-sm-12 products-box">
 				<div class="col-md-12">
 					<div class="single-product single-product-toggle" data-toggle="tooltip" data-placement="top"
-					     title="Click For Mode Details"
+					     title="Click For More Details"
 					     style="background-image: url(<?php echo get_template_directory_uri() . '/assets/images/original/clareanne-residential-1500x998.png'; ?>);background-position-y: center;">
 						<h4>KITCHEN</h4>
 					</div>
@@ -39,22 +39,36 @@ get_header(); ?>
 					</div>
 				</div>
 				<div class="col-md-12">
-					<div class="single-product"
+					<div class="single-product single-product-toggle" data-toggle="tooltip" data-placement="top"
+					     title="Click For More Details"
 					     style="background-image: url(<?php echo get_template_directory_uri() . '/assets/images/original/Seagrove_Kirkstead_B_cambriastyle_001_14_1260x960.png'; ?>);background-position-y: 40%;">
 						<h4>BATH</h4>
 					</div>
+					<div class="single-product-content">
+						Today’s bathroom serves as an oasis from everyday stress. At Case Supply, we take luxury bathing to a new level. Relaxing whirlpools, steam showers, and innovative bodyspray systems have made bath remodeling more exciting than ever.
+						<br><br>
+						If you’re looking to make a small bathroom more functional, Case Supply can help you as well. Water closets and pedestal lavatories together in stylish ensembles can create a prestigious look on any budget.
+						<br><br>
+						At Case Supply, bathrooms are our business!
+					</div>
 				</div>
 				<div class="col-md-12">
-					<div class="single-product"
+					<div class="single-product single-product-toggle" data-toggle="tooltip" data-placement="top"
+					     title="Click For More Details"
 					     style="background-image: url(<?php echo get_template_directory_uri() . '/assets/images/original/brittanicca_t_khachibrandonbarre_001_16_1260x960.png'; ?>);background-position-y: 85%;">
 						<h4>COUNTERTOPS</h4>
 					</div>
+					<div class="single-product-content">
+						Case Supply proudly offers a variety of granite, solid-surface, quartz, and laminate countertop products.
+					</div>
 				</div>
 				<div class="col-md-12">
-					<div class="single-product"
+					<div class="single-product single-product-toggle" data-toggle="tooltip" data-placement="top"
+					     title="Click For More Details"
 					     style="background-image: url(<?php echo get_template_directory_uri() . '/assets/images/original/stove.png'; ?>);background-position-y: 15%;">
 						<h4>APPLIANCES</h4>
 					</div>
+					<div class="single-product-content"></div>
 				</div>
 			</div>
 		</div>
