5535385b323b9d14a2624c8c1385ff83305323ea
Ken Vilar <kenvilar@gmail.com>
2017-10-23 09:39:25 +0800

    Adjust banner height

--- front-page.php
+++ front-page.php
@@ -13,20 +13,20 @@ get_header(); ?>
 </section>
 
 <section id="home-services">
-	<div class="container-fluid">
+	<div class="container">
 		<div class="row">
-			<div class="text-center single-service col-md-4 col-sm-12 col-xs-12">
+			<div class="text-center single-service col-md-4 col-sm-12 col-xs-12 equal-height">
 				<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/design.png'; ?>" alt="">
 				<h3>Design</h3>
 				<p>Our certified designers will come to you in the comfort of your home to design the kitchen or bath of
 					your dreams.</p>
 			</div>
-			<div class="text-center single-service col-md-4 col-sm-12 col-xs-12">
+			<div class="text-center single-service col-md-4 col-sm-12 col-xs-12 equal-height">
 				<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/installation.png'; ?>" alt="">
 				<h3>INSTALLATION</h3>
 				<p>Case Supply’s professional contractors will complete your project to perfection.</p>
 			</div>
-			<div class="text-center single-service col-md-4 col-sm-12 col-xs-12">
+			<div class="text-center single-service col-md-4 col-sm-12 col-xs-12 equal-height">
 				<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/delivery.png'; ?>" alt="">
 				<h3>DELIVERY</h3>
 				<p>Our experts will make sure your products are delivered safely and on time to your doorstep.</p>
