2ff0f6c0e3ea12eee3384c8279797c25fc2714e8
Ken Vilar <kenvilar@gmail.com>
2017-10-23 19:08:11 +0800

    Add if-else conditional using ACF in footer

--- footer.php
+++ footer.php
@@ -21,27 +21,50 @@
 		<div class="container-fluid color-white">
 			<div class="row">
 				<div class="col-md-offset-1 col-md-10 col-sm-12">
+	
 					<div class="col-md-4 col-sm-4 col-xs-12 single background-dark-gray">
-						<a href="mailto:information@casesupply.com">
-							<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/email.png'; ?>"
-							     alt="icon">
-							<p>information@casesupply.com</p>
-						</a>
+						<?php if ( get_field( 'email', 'option' ) ) : ?>
+							<a href="mailto:<?php the_field( 'email', 'option' ); ?>">
+								<img src="<?php the_field( 'email_icon', 'option' ); ?>" alt="icon">
+								<p><?php the_field( 'email', 'option' ); ?></p>
+							</a>
+						<?php else : ?>
+							<a href="mailto:information@casesupply.com">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/email.png'; ?>"
+								     alt="icon">
+								<p>information@casesupply.com</p>
+							</a>
+						<?php endif; ?>
 					</div>
 					<div class="col-md-4 col-sm-4 col-xs-12 single background-green">
-						<a href="tel:315.425.1818">
-							<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/call.png'; ?>"
-							     alt="icon">
-							<p>315.425.1818</p>
-						</a>
+						<?php if ( get_field( 'phone', 'option' ) ) : ?>
+							<a href="tel:<?php the_field( 'phone', 'option' ); ?>">
+								<img src="<?php the_field( 'phone_icon', 'option' ); ?>" alt="icon">
+								<p><?php the_field( 'phone', 'option' ); ?></p>
+							</a>
+						<?php else : ?>
+							<a href="tel:315.425.1818">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/call.png'; ?>"
+								     alt="icon">
+								<p>315.425.1818</p>
+							</a>
+						<?php endif; ?>
 					</div>
 					<div class="col-md-4 col-sm-4 col-xs-12 single background-blue">
-						<a href="https://www.facebook.com/casesupplyinc/">
-							<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/facebook.png'; ?>"
-							     alt="icon">
-							<p>Find us on Facebook</p>
-						</a>
+						<?php if ( get_field( 'facebook', 'option' ) ) : ?>
+							<a href="<?php the_field( 'facebook', 'option' ); ?>">
+								<img src="<?php the_field( 'facebook_icon', 'option' ); ?>" alt="icon">
+								<p>Find us on Facebook</p>
+							</a>
+						<?php else : ?>
+							<a href="https://www.facebook.com/casesupplyinc/">
+								<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/facebook.png'; ?>"
+								     alt="icon">
+								<p>Find us on Facebook</p>
+							</a>
+						<?php endif; ?>
 					</div>
+	
 				</div>
 			</div>
 		</div>
