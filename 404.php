8e99850fd1fd0fcca024bd537691f398830d0805
Ken Vilar <kenvilar@gmail.com>
2017-10-24 20:31:24 +0800

    Change 404 template

--- 404.php
+++ 404.php
@@ -2,59 +2,37 @@
 /**
  * The template for displaying 404 pages (not found)
  *
- * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ * @link    https://codex.wordpress.org/Creating_an_Error_404_Page
  *
  * @package casesupply
  */
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+	<section id="primary" class="content-area">
+		<div class="container" id="main">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'casesupply' ); ?></h1>
-				</header><!-- .page-header -->
+			<div class="row error-404 not-found">
+				<div class="col-md-6 col-sm-6 col-xs-12">
+					<br><br>
+					<img class="img-responsive center-block"
+					     src="<?php echo get_template_directory_uri() . '/assets/images/404.png'; ?>" alt="404">
+				</div>
+				<div class="col-md-6 col-sm-6 col-xs-12 text-center">
+					<header class="page-header">
+						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'casesupply' ); ?></h1>
+					</header>
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links above?', 'casesupply' ); ?></p>
+				</div>
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'casesupply' ); ?></p>
+				<div class="col-md-12 col-xs-12 page-content">
 
-					<?php
-						get_search_form();
+				</div>
+			</div>
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'casesupply' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'casesupply' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</div>
+	</section>
+	<br><br>
 
 <?php
 get_footer();
