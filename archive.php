843f42210dfe4c236849ee453f6039987cfe52f0
Ken Vilar <kenvilar@gmail.com>
2017-10-25 02:18:32 +0800

    Modify archive page

--- archive.php
+++ archive.php
@@ -2,7 +2,7 @@
 /**
  * The template for displaying archive pages
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * @link    https://codex.wordpress.org/Template_Hierarchy
  *
  * @package casesupply
  */
@@ -10,41 +10,35 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+		<div id="main" class="container site-main">
+			
+			<?php
+			if ( have_posts() ) : ?>
 
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
+				<header class="page-header">
+					<?php
 					the_archive_title( '<h1 class="page-title">', '</h1>' );
 					the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+					?>
+				</header>
+				
+				<?php
+				while ( have_posts() ) : the_post();
+					
+					get_template_part( 'template-parts/content', get_post_format() );
+				
+				endwhile;
+				
+				the_posts_navigation();
+			
+			else :
+				
+				get_template_part( 'template-parts/content', 'none' );
+			
+			endif; ?>
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+		</div>
+	</div>
 
 <?php
 get_sidebar();
