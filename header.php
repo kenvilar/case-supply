b823d8c208f2ea413fba25d6afec42d4d499b5b0
Ken Vilar <kenvilar@gmail.com>
2017-10-26 02:08:02 +0800

    Add favicon image

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="icon" href="<?php echo get_template_directory_uri() . '/assets/images/favicon.png'; ?>" sizes="any" type="image/png">
 	
 	<?php wp_head(); ?>
 	
