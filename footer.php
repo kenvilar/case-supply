ef0d2fed1a2b6c7693185a0cfa57c0fc0e949c02
Ken Vilar <kenvilar@gmail.com>
2017-10-25 22:06:23 +0800

    Add background image handler in get in touch section

--- footer.php
+++ footer.php
@@ -11,76 +11,103 @@
 
 ?>
 
-	<section id="template-container" class="get-in-touch text-center">
-		<div class="container">
-			<div class="row">
-				<p class="header1 header">GET IN TOUCH</p>
-				<p class="header5">We'd love to hear from you!</p><br>
-			</div>
+<section id="template-container" class="get-in-touch text-center">
+	<div class="container">
+		<div class="row">
+			<p class="header1 header">GET IN TOUCH</p>
+			<p class="header5">We'd love to hear from you!</p><br>
 		</div>
-		<div class="container-fluid color-white">
-			<div class="row">
-				<div class="col-md-offset-1 col-md-10 col-sm-12">
-	
-					<div class="col-md-4 col-sm-4 col-xs-12 single background-dark-gray">
-						<?php if ( get_field( 'email', 'option' ) ) : ?>
-							<a href="mailto:<?php the_field( 'email', 'option' ); ?>">
-								<img src="<?php the_field( 'email_icon', 'option' ); ?>" alt="icon">
-								<p><?php the_field( 'email', 'option' ); ?></p>
-							</a>
-						<?php else : ?>
-							<a href="mailto:information@casesupply.com">
-								<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/email.png'; ?>"
-								     alt="icon">
-								<p>information@casesupply.com</p>
-							</a>
-						<?php endif; ?>
-					</div>
-					<div class="col-md-4 col-sm-4 col-xs-12 single background-green">
-						<?php if ( get_field( 'phone', 'option' ) ) : ?>
-							<a href="tel:<?php the_field( 'phone', 'option' ); ?>">
-								<img src="<?php the_field( 'phone_icon', 'option' ); ?>" alt="icon">
-								<p><?php the_field( 'phone', 'option' ); ?></p>
-							</a>
-						<?php else : ?>
-							<a href="tel:315.425.1818">
-								<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/call.png'; ?>"
-								     alt="icon">
-								<p>315.425.1818</p>
-							</a>
-						<?php endif; ?>
-					</div>
-					<div class="col-md-4 col-sm-4 col-xs-12 single background-blue">
-						<?php if ( get_field( 'facebook', 'option' ) ) : ?>
-							<a href="<?php the_field( 'facebook', 'option' ); ?>">
-								<img src="<?php the_field( 'facebook_icon', 'option' ); ?>" alt="icon">
-								<p>Find us on Facebook</p>
-							</a>
-						<?php else : ?>
-							<a href="https://www.facebook.com/casesupplyinc/">
-								<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/facebook.png'; ?>"
-								     alt="icon">
-								<p>Find us on Facebook</p>
-							</a>
-						<?php endif; ?>
-					</div>
-	
+	</div>
+	<div class="container-fluid color-white">
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 col-sm-12">
+
+				<div class="col-md-4 col-sm-4 col-xs-12 single background-dark-gray"
+				     style="background: linear-gradient(rgba(164, 164, 164, 0.80), rgba(164, 164, 164, 0.80)),
+						     url(
+				     <?php
+				     if ( get_field( 'email_background_image_url', 'option' ) ) :
+					     the_field( 'email_background_image_url', 'option' );
+				     else :
+					     echo get_template_directory_uri() . '/assets/images/get-in-touch/Summerhill_K_BHG_MEQUON_005_15-email.jpg';
+				     endif; ?>);">
+					
+					<?php if ( get_field( 'email', 'option' ) ) : ?>
+						<a href="mailto:<?php the_field( 'email', 'option' ); ?>">
+							<img src="<?php the_field( 'email_icon', 'option' ); ?>" alt="icon">
+							<p><?php the_field( 'email', 'option' ); ?></p>
+						</a>
+					<?php else : ?>
+						<a href="mailto:information@casesupply.com">
+							<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/email.png'; ?>"
+							     alt="icon">
+							<p>information@casesupply.com</p>
+						</a>
+					<?php endif; ?>
 				</div>
-			</div>
-		</div>
-	</section>
-	
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="text-center col-md-12">
-					<a href="<?php echo esc_url( __( '/', 'casesupply' ) ); ?>">
-						<?php printf( esc_html__( '&copy; %s, INC. ALL RIGHTS RESERVED', 'casesupply' ), 'CASE SUPPLY' ); ?>
-					</a>
+				<div class="col-md-4 col-sm-4 col-xs-12 single background-green"
+				     style="background: linear-gradient(rgba(54, 158, 105, 0.80), rgba(54, 158, 105, 0.80)),
+						     url(
+				     <?php
+				     if ( get_field( 'phone_background_image_url', 'option' ) ) :
+					     the_field( 'phone_background_image_url', 'option' );
+				     else :
+					     echo get_template_directory_uri() . '/assets/images/get-in-touch/Vanity62-phone.jpg';
+				     endif; ?>);">
+					
+					<?php if ( get_field( 'phone', 'option' ) ) : ?>
+						<a href="tel:<?php the_field( 'phone', 'option' ); ?>">
+							<img src="<?php the_field( 'phone_icon', 'option' ); ?>" alt="icon">
+							<p><?php the_field( 'phone', 'option' ); ?></p>
+						</a>
+					<?php else : ?>
+						<a href="tel:315.425.1818">
+							<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/call.png'; ?>"
+							     alt="icon">
+							<p>315.425.1818</p>
+						</a>
+					<?php endif; ?>
 				</div>
+				<div class="col-md-4 col-sm-4 col-xs-12 single background-blue"
+				     style="background: linear-gradient(rgba(95, 154, 224, 0.80), rgba(95, 154, 224, 0.80)),
+						     url(
+				     <?php
+				     if ( get_field( 'facebook_background_image_url', 'option' ) ) :
+					     the_field( 'facebook_background_image_url', 'option' );
+				     else :
+					     echo get_template_directory_uri() . '/assets/images/get-in-touch/clareanne-residential-1500x998-fb.jpg';
+				     endif; ?>);">
+					
+					<?php if ( get_field( 'facebook', 'option' ) ) : ?>
+						<a href="<?php the_field( 'facebook', 'option' ); ?>">
+							<img src="<?php the_field( 'facebook_icon', 'option' ); ?>" alt="icon">
+							<p>Find us on Facebook</p>
+						</a>
+					<?php else : ?>
+						<a href="https://www.facebook.com/casesupplyinc/">
+							<img src="<?php echo get_template_directory_uri() . '/assets/images/icons/facebook.png'; ?>"
+							     alt="icon">
+							<p>Find us on Facebook</p>
+						</a>
+					<?php endif; ?>
+				</div>
+
 			</div>
 		</div>
-	</footer>
+	</div>
+</section>
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="text-center col-md-12">
+				<a href="<?php echo esc_url( __( '/', 'casesupply' ) ); ?>">
+					<?php printf( esc_html__( '&copy; %s, INC. ALL RIGHTS RESERVED', 'casesupply' ), 'CASE SUPPLY' ); ?>
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
 
 </div><!-- #main-container -->
 
