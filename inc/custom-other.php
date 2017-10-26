d2933052d93a34298bc70cf6944f5f5fa03965c2
Ken Vilar <kenvilar@gmail.com>
2017-10-26 16:54:43 +0800

    Remove negate in if exif_imagetype exists

--- inc/custom-other.php
+++ inc/custom-other.php
@@ -6,7 +6,7 @@ remove_filter( 'the_content', 'wpautop' );
 /* Check if the existing file is an image file */
 function is_image_file( $image_file ) {
 	
-	if ( ! function_exists( 'exif_imagetype' ) ) {
+	if ( function_exists( 'exif_imagetype' ) ) {
 		if ( exif_imagetype( $image_file ) == IMAGETYPE_GIF ) {
 			return true;
 		} else if ( exif_imagetype( $image_file ) == IMAGETYPE_JPEG ) {
