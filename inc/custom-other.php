4da9f5950dc61393124f47277483608421660d0f
Ken Vilar <kenvilar@gmail.com>
2017-10-26 16:35:02 +0800

    Create function that checks if it’s an image file

--- inc/custom-other.php
+++ inc/custom-other.php
@@ -2,3 +2,22 @@
 
 /* disable the wpautop */
 remove_filter( 'the_content', 'wpautop' );
+
+/* Check if the existing file is an image file */
+function is_image_file( $image_file ) {
+	
+	if ( ! function_exists( 'exif_imagetype' ) ) {
+		if ( exif_imagetype( $image_file ) == IMAGETYPE_GIF ) {
+			return true;
+		} else if ( exif_imagetype( $image_file ) == IMAGETYPE_JPEG ) {
+			return true;
+		} else if ( exif_imagetype( $image_file ) == IMAGETYPE_PNG ) {
+			return true;
+		} else if ( exif_imagetype( $image_file ) == IMAGETYPE_ICO ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+}
