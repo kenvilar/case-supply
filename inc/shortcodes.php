b5d99b677b5a0d6771be2ef3d62e8a8636de44d1
Ken Vilar <kenvilar@gmail.com>
2017-10-26 16:35:22 +0800

    Apply is_image_file function to image shortcode

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -3,7 +3,6 @@
 // Grid Columns
 function casesupply_grid_shortcode( $atts, $content = null ) {
 	
-	// Attributes
 	$atts = shortcode_atts(
 		array(
 			'class' => 'col-lg-12 col-md-12 col-sm-12 col-xs-12',
@@ -35,3 +34,76 @@ function casesupply_grid_shortcode( $atts, $content = null ) {
 }
 
 add_shortcode( 'grid', 'casesupply_grid_shortcode' );
+
+// Image Shortcode
+function casesupply_image_shortcode( $atts, $content = null ) {
+	
+	$atts = shortcode_atts(
+		array(
+			'href'   => "",
+			'width'  => 'auto',
+			'height' => 'auto',
+			'alt'    => 'image',
+		),
+		$atts,
+		'image'
+	);
+	
+	if ( "" == $atts['href'] ) {
+		$atts['href'] = "";
+	}
+	
+	if ( "" == $atts['width'] ) {
+		$atts['width'] = "auto";
+	}
+	
+	if ( "" == $atts['height'] ) {
+		$atts['height'] = "auto";
+	}
+	
+	if ( "" == $atts['alt'] ) {
+		$atts['alt'] = "image";
+	}
+	
+	$casesupply_image = '<a';
+	
+	if ( isset( $atts['href'] ) || "" !== $atts['href'] ) :
+		$casesupply_image .= ' href="' . $atts['href'] . '">';
+	else :
+		$casesupply_image .= '>';
+	endif;
+	
+	$casesupply_image .= '<img ';
+	
+	if ( ( isset( $content ) || "" !== $content ) && is_image_file( $content ) ) {
+		$casesupply_image .= 'src="' . do_shortcode( $content ) . '" ';
+	} else {
+		$casesupply_image .= 'src="http://via.placeholder.com/250x250?text=put+image+url" ';
+	}
+	
+	if ( isset( $atts['alt'] ) || "" !== $atts['alt'] ) :
+		$casesupply_image .= 'alt="' . $atts['alt'] . '" ';
+	else :
+		$casesupply_image .= 'alt="' . $atts['alt'] . '" ';
+	endif;
+	
+	if ( isset( $atts['width'] ) || "" !== $atts['width'] ) :
+		$casesupply_image .= 'style="width:' . $atts['width'] . ';';
+	else :
+		$casesupply_image .= 'style="width:' . $atts['width'] . ';';
+	endif;
+	
+	if ( isset( $atts['height'] ) || "" !== $atts['height'] ) :
+		$casesupply_image .= 'height:' . $atts['height'] . ';">';
+	else :
+		$casesupply_image .= 'height:' . $atts['height'] . ';">';
+	endif;
+	
+	
+	$casesupply_image .= '</a>';
+	
+	return $casesupply_image;
+	
+}
+
+add_shortcode( 'image', 'casesupply_image_shortcode' );
