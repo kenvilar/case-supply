c4a8b85056c1b184a76ee7e31449f5683b133c1b
Ken Vilar <kenvilar@gmail.com>
2017-11-01 00:47:25 +0800

    Remove the products contents

--- templates/tpl-products.php
+++ templates/tpl-products.php
@@ -49,20 +49,6 @@ get_header(); ?>
 						</div>
 						<div class="single-product-content">
 							<h2 class="product-title text-center">KITCHEN</h2>
-							For most people, renovating a kitchen is a once-in-a-lifetime experience. Case Supply
-							recognizes
-							the importance of your project, and understands the details surrounding each decision. That
-							is
-							why Case Supply represents only elite manufacturers of cabinetry, appliances and fixtures,
-							with
-							styles to fit every budget and every dream.
-							<br><br>
-							Certified Kitchen Designers will walk you through each phase of work, from elevations to
-							three-dimensional perspectives, from field measurement to implementation. Case Supply even
-							has a
-							referral network.
-							<br><br>
-							Case Supply wants your kitchen project to be special!
 						</div>
 					</div>
 					<div class="col-md-12">
@@ -72,18 +58,6 @@ get_header(); ?>
 						</div>
 						<div class="single-product-content">
 							<h2 class="product-title text-center">BATH</h2>
-							Today’s bathroom serves as an oasis from everyday stress. At Case Supply, we take luxury
-							bathing
-							to a new level. Relaxing whirlpools, steam showers, and innovative bodyspray systems have
-							made
-							bath remodeling more exciting than ever.
-							<br><br>
-							If you’re looking to make a small bathroom more functional, Case Supply can help you as
-							well.
-							Water closets and pedestal lavatories together in stylish ensembles can create a prestigious
-							look on any budget.
-							<br><br>
-							At Case Supply, bathrooms are our business!
 						</div>
 					</div>
 					<div class="col-md-12">
@@ -93,22 +67,6 @@ get_header(); ?>
 						</div>
 						<div class="single-product-content">
 							<h2 class="product-title text-center">COUNTERTOPS</h2>
-							Case Supply proudly offers a variety of granite, solid-surface, quartz, and laminate
-							countertop
-							products.
-							<br><br>
-							<div class="col-md-3 col-sm-6 col-xs-6 equal-height"><img
-										src="<?php echo get_template_directory_uri() . '/assets/images/corian-design.png'; ?>"
-										alt="image" style="width:auto!important;height:auto!important;"></div>
-							<div class="col-md-3 col-sm-6 col-xs-6 equal-height"><img
-										src="<?php echo get_template_directory_uri() . '/assets/images/hartson-kennedy.png'; ?>"
-										alt="image" style="width:auto!important;height:auto!important;"></div>
-							<div class="col-md-3 col-sm-6 col-xs-6 equal-height"><img
-										src="<?php echo get_template_directory_uri() . '/assets/images/cambria.png'; ?>"
-										alt="image" style="width:auto!important;height:auto!important;"></div>
-							<div class="col-md-3 col-sm-6 col-xs-6 equal-height"><img
-										src="<?php echo get_template_directory_uri() . '/assets/images/busch.png'; ?>"
-										alt="image" style="width:auto!important;height:auto!important;"></div>
 						</div>
 					</div>
 					<div class="col-md-12">
@@ -118,8 +76,6 @@ get_header(); ?>
 						</div>
 						<div class="single-product-content">
 							<h2 class="product-title text-center">APPLIANCES</h2>
-							VISIT OUR UPDATED SHOWROOM <br>
-							Check out our newly remodeled showroom for the latest trends in Kitchen & Bath styles!
 						</div>
 					</div>
 				<?php endif;
