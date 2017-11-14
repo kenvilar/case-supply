179ee6a8ec12bc61defa6c9a535ae08f65170fe4
Ken Vilar <kenvilar@gmail.com>
2017-11-15 00:08:16 +0800

    Fix the default content for product template

--- templates/tpl-products.php
+++ templates/tpl-products.php
@@ -57,39 +57,63 @@ get_header(); ?>
 							<div class="container">
 								<div class="row">
 									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
-										<img class="img-responsive"
-										     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Monarch.png"
-										     alt="products-image" style="width:auto !important;height:auto !important;">
+										<a href="http://www.bjtidwell.com/" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Monarch.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
 									</div>
 									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
-										<img class="img-responsive"
-										     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Dura-Supreme.png"
-										     alt="products-image" style="width:auto !important;height:auto !important;">
+										<a href="http://www.durasupreme.com/" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Dura-Supreme.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
 									</div>
 									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
-										<img class="img-responsive"
-										     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Kitchen-Kompact.png"
-										     alt="products-image" style="width:auto !important;height:auto !important;">
+										<a href="http://kitchenkompact.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Kitchen-Kompact.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
 									</div>
 									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
-										<img class="img-responsive"
-										     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Medallion-Gold.png"
-										     alt="products-image" style="width:auto !important;height:auto !important;">
+										<a href="http://www.medallioncabinetry.com/" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Medallion-Gold.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
 									</div>
 									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
-										<img class="img-responsive"
-										     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/RiverRun.png"
-										     alt="products-image" style="width:auto !important;height:auto !important;">
+										<a href="http://www.riverruncabinetry.com/" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/RiverRun.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
 									</div>
 									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
-										<img class="img-responsive"
-										     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Wolf-Products.png"
-										     alt="products-image" style="width:auto !important;height:auto !important;">
+										<a href="http://www.wolfhomeproducts.com/" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/Wolf-Products.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
 									</div>
 									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
-										<img class="img-responsive"
-										     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/WoodPro.png"
-										     alt="products-image" style="width:auto !important;height:auto !important;">
+										<a href="http://www.woodpro.com/" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/cabinets/WoodPro.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
 									</div>
 								</div>
 							</div>
@@ -102,6 +126,84 @@ get_header(); ?>
 						</div>
 						<div class="single-product-content">
 							<h2 class="product-title text-center">FIXTURES</h2>
+							<div class="container">
+								<div class="row">
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://aquaticbath.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Aquatic.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.elkay.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Elkay.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.gerberonline.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Gerber.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.insinkerator.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Insinkerator.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.us.kohler.com/us/" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Kohler.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.moen.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Moen2.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.mrsteam.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Mr.-Steam.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.sterlingplumbing.com" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Sterling.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.swanstone.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/fixture/Swan.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 					<div class="col-md-12">
@@ -111,6 +213,70 @@ get_header(); ?>
 						</div>
 						<div class="single-product-content">
 							<h2 class="product-title text-center">COUNTERTOPS</h2>
+							<div class="container">
+								<div class="row">
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.buschproducts.com" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/countertops/Busch.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.cambriausa.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/countertops/Cambria2.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.dupont.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/countertops/DuPont.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.formica.com/en/us/homeowner" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/countertops/Formica.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.hartson-kennedy.com" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/countertops/Hartson-Kennedy.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.cosentino.com/sensa" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/countertops/Cosentino.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.wilsonart.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/countertops/Wilsonart2.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 					<div class="col-md-12">
@@ -120,6 +286,68 @@ get_header(); ?>
 						</div>
 						<div class="single-product-content">
 							<h2 class="product-title text-center">MISCELLANEOUS</h2>
+							<div class="container">
+								<div class="row">
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.bascoshowerdoor.com" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/miscellaneous/Basco.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.besttile.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/miscellaneous/Best-Tile.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.broan-nutone.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/miscellaneous/Broan_NuTone.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.geappliances.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/miscellaneous/General-Electric.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.rangairemfg.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/miscellaneous/Rangaire.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.remeecasting.com" target="_blank" rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/miscellaneous/Remee-Casting.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+									<div class="col-md-3 col-sm-6 col-xs-6 equal-height">
+										<a href="http://www.richelieu.com/us/en" target="_blank"
+										   rel="noopener noreferrer">
+											<img class="img-responsive"
+											     src="<?php echo get_template_directory_uri(); ?>/assets/images/products/miscellaneous/Richelieu.png"
+											     alt="products-image"
+											     style="width:auto !important;height:auto !important;">
+										</a>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				<?php endif;
