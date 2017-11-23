cee40de4c531a5315041e1a322125421962135fa
Ken Vilar <kenvilar@gmail.com>
2017-11-23 21:05:59 +0800

    Fix for theme template file caching

--- inc/custom-other.php
+++ inc/custom-other.php
@@ -56,3 +56,26 @@ function casesupply_add_dashboard_widgets() {
 }
 
 add_action( 'wp_dashboard_setup', 'casesupply_add_dashboard_widgets' );
+
+/*
+ * Fix for theme template file caching.
+ * Flush the theme file cache each time the admin screens are loaded which uses the file list.
+ * Only flush the file cache with each request to post list table, edit post screen, or theme editor.
+ * */
+function wp_42573_fix_template_caching( WP_Screen $current_screen ) {
+	if ( ! in_array( $current_screen->base, array( 'post', 'edit', 'theme-editor' ), true ) ) {
+		return;
+	}
+	
+	$theme = wp_get_theme();
+	if ( ! $theme ) {
+		return;
+	}
+	
+	$cache_hash    = md5( $theme->get_theme_root() . '/' . $theme->get_stylesheet() );
+	$label         = sanitize_key( 'files_' . $cache_hash . '-' . $theme->get( 'Version' ) );
+	$transient_key = substr( $label, 0, 29 ) . md5( $label );
+	delete_transient( $transient_key );
+}
+
+add_action( 'current_screen', 'wp_42573_fix_template_caching' );
